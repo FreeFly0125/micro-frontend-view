60a86e020566a2364769a9396bf41463978b4af0
FreeFly0125 <simonbergman516@gmail.com>
2024-06-15 19:27:04 +0200

    initialize element position

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Draggable from "react-draggable";
 import styled from "styled-components";
 
@@ -19,14 +19,24 @@ const MyButton = styled.button`
 `;
 
 export const DragElement: React.FC = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const updatePosition = (e: any, data: any) => {
+    setPosition({ x: data.x, y: data.y });
+  };
+
+  const resetPosition = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
   return (
     <div>
       <h1>Draggable Element</h1>
       <div style={{ margin: "10px" }}>
-        <MyButton>Reset Position</MyButton>
+        <MyButton onClick={resetPosition}>Reset Position</MyButton>
       </div>
       <div>
-        <Draggable>
+        <Draggable position={position} onDrag={updatePosition}>
           <MyDiv>Hello World!</MyDiv>
         </Draggable>
       </div>
